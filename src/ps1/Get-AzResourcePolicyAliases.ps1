a4097e34aecf93c891dd371f32b91762274baba1
Mats Estensen <matsest@mxe.no>
2022-06-17 22:54:01 +0200

    fix: add output path

--- src/ps1/Get-AzResourcePolicyAliases.ps1
+++ src/ps1/Get-AzResourcePolicyAliases.ps1
@@ -1,24 +1,26 @@
-# Originial by https://github.com/maciejporebski/azure-policy-aliases/blob/master/src/Get-ResourceAliases.ps1 (maciejporebski)
+# Based on originial by https://github.com/maciejporebski/azure-policy-aliases/blob/master/src/Get-ResourceAliases.ps1 (maciejporebski)
 
+$repoRoot = & git rev-parse --show-toplevel
+$basePath = "$repoRoot/docs"
+if (!(Test-Path -Path $basePath)) {
+    New-Item -Path $repoRoot -Name 'docs' -ItemType "directory" | Out-Null
+}
+
+# Find providers
 $resourceTypes = Get-AzPolicyAlias -ListAvailable
 $providers = $resourceTypes | Group-Object -Property Namespace
 
-$basePath = "aliases"
-if (!(Test-Path -Path $basePath)) {
-    New-Item -Name $basePath -ItemType "directory"
-}
-
-$toc = "# Azure Policy Aliases`n"
-$toc += "This repository contains all available resource property aliases for easy reference when creating Policy definitions. The data is periodically fetched using Get-AzPolicyAlias command provided as part of the Az Module.`n`n"
+$content = "# Azure Policy Aliases`n"
 
 foreach ($provider in $providers) {
     $resourceTypesWithAliases = $provider.Group | Where-Object { $_.Aliases.Count -gt 0 }
+
     if ($resourceTypesWithAliases.Count -gt 0) {
-        $toc += "## $($provider.Name)`n`n"
-        
+        $content += "## $($provider.Name)`n"
+
         $namespacePath = "$($basePath)/$($provider.Name)"
         if (!(Test-Path -Path $namespacePath)) {
-            New-Item -Path $basePath -Name $provider.Name -ItemType "directory"
+            New-Item -Path $basePath -Name $provider.Name -ItemType "directory" | Out-Null
         }
 
         foreach ($resourceType in $resourceTypesWithAliases) {
@@ -31,11 +33,11 @@ foreach ($provider in $providers) {
             $filePath = "$($namespacePath)/$($fileName).md"
             $resourceMarkdown | Out-File -FilePath $filePath
 
-            $toc += "- [$($resourceType.Namespace)/$($resourceType.ResourceType)]($filePath)`n"
+            $content += "- [$($resourceType.Namespace)/$($resourceType.ResourceType)]($filePath)`n"
         }
 
-        $toc += "`n`n"
+        $content += "`n"
 
-        $toc | Out-File -FilePath "README.md"
+        $content | Out-File -FilePath "$basePath/README.md"
     }
 }
