7c58830691ececb00c3d0b03cc6fb90080397563
Mats Estensen <matsest@mxe.no>
2024-09-29 23:55:48 +0200

    fix: update-theme.sh

--- src/update-theme.sh
+++ src/update-theme.sh
@@ -2,6 +2,25 @@
 
 # https://geekdocs.de/usage/getting-started/#option-1-download-pre-build-release-bundle
 
+# Variables
 ROOT=$(git rev-parse --show-toplevel)
-mkdir -p themes/hugo-geekdoc/
-curl -L https://github.com/thegeeklab/hugo-geekdoc/releases/latest/download/hugo-geekdoc.tar.gz | tar -xz -C $ROOT/themes/hugo-geekdoc/ --strip-components=1
+THEME_PATH="$ROOT/themes/hugo-geekdoc/"
+
+# Clean old theme
+echo "Removing old theme"
+rm -rf $THEME_PATH
+
+# Download latest release
+echo "Downloading latest version of theme"
+mkdir -p $THEME_PATH
+curl -Ls https://github.com/thegeeklab/hugo-geekdoc/releases/latest/download/hugo-geekdoc.tar.gz | tar -xz -C $THEME_PATH --strip-components=1
+
+# Check for changes
+if [[ $(git status --porcelain -- $THEME_PATH) ]]; then
+  # Changes
+  echo "Theme updated with changes!"
+  NEW_VERSION=$(cat $THEME_PATH/VERSION)
+  echo "New version: $NEW_VERSION"
+else
+  echo "No changes detected"
+fi
